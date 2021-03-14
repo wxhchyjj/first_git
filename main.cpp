64030b4b989690b3c662b77dd34cec1f061bd06c
lihuiling <lihuiling@lihuilingdeMacBook-Pro.local>
2021-03-14 08:40:01 +0100

    chy

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+
+#include"litteral.cpp"
+#include<QString>
+#include<QDebug>
+#include<iostream>
+using namespace std;
+int main(){
+Litterale* l= distinguerConstruire("SR01");
+qDebug()<<l<<endl;
+}
+
